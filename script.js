6c00f5ba511a7a6f527ff02847d04a6cc92a562e
ph773716 <phone773716@gmail.com>
2025-06-26 00:25:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-function togglePassword() {
-  const password = document.getElementById("password");
-  if (password.type === "password") {
-    password.type = "text";
-  } else {
-    password.type = "password";
-  }
-}
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
